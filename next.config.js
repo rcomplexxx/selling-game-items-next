5c01e0fe572e49b291766fb3f5558df66e6b754f
unknown <rcomplexx@gmail.com>
2023-05-23 04:52:46 +0200

    Ready for deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    distDir: '_static',
-    images: {
-      unoptimized: true
-    }
+    
 }
 
 module.exports = nextConfig
