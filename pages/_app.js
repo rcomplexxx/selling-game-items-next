0bc23e7faa3323df42b7e59007f0bcb07dcd8547
unknown <rcomplexx@gmail.com>
2023-05-25 08:49:47 +0200

    bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -4,35 +4,33 @@ import Navbar from "../components/Navbar/Navbar.jsx";
 import AppContext from '@/contexts/AppContext';
 import Footer from '@/components/Footer/Footer';
 // import { commerce } from "./lib/commerce";
+import React, { useState, useEffect } from "react";
 
-export default function App({ Component, pageProps }) {
-  const [mobileOpen, setMobileOpen] = useState(false);
-  const [cartProducts, setCartProducts] = useState([]);
-  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
-
+export default function App() {
   const [hasScrollbar, setHasScrollbar] = useState(false);
 
   useEffect(() => {
-    const bodyStyle = window.getComputedStyle(document.body);
-    const hasScrollbar = bodyStyle.getPropertyValue('scrollbar-width') === 'thin';
-    setHasScrollbar(hasScrollbar);
+    const hasVerticalScrollbar =
+      document.documentElement.scrollHeight >
+      document.documentElement.clientHeight;
+    setHasScrollbar(hasVerticalScrollbar);
   }, []);
 
   return (
-    <>
-      <div
-        id="hronika"
-        style={{
-          width: hasScrollbar ? 'calc(100% - 10px)' : '100%',
-          height: "fit-content",
-          display: "flex",
-          flexDirection: "column",
-          backgroundColor: "black",
-          color: "white",
-          minHeight: "100vh",
-          minWidth: "100vw",
-        }}
-      >
+    <div
+      id="hronika"
+      style={{
+        width: hasScrollbar ? "calc(100% - 10px)" : "100%",
+        height: "fit-content",
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "black",
+        color: "white",
+        minHeight: "100vh",
+        minWidth: "100vw",
+      }}
+    >
+ 
         <Navbar
           totalItems={cartProducts.length}
           handleDrawerToggle={handleDrawerToggle}
@@ -43,7 +41,7 @@ export default function App({ Component, pageProps }) {
         </AppContext.Provider>
 
         <Footer/>
-      </div>
-    </>
+        </div>
   );
+
 }
