d2a2bd213a1164abdad5324b4e287d5ebd5cd068
unknown <rcomplexx@gmail.com>
2023-05-25 09:49:37 +0200

    bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -34,6 +34,7 @@ export default function App({ Component, pageProps }) {
         marginRight:hasScrollbar ? "10px" : "0",
       }}
     >
+      {hasScrollbar && <span>Hello</span>}
  
         <Navbar
           totalItems={cartProducts.length}
