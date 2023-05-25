04e6f282641324ce3235167e35b1d68a6770a4d8
unknown <rcomplexx@gmail.com>
2023-05-25 10:24:46 +0200

    bug fix

--- components/Cards/PolicyCard/PolicyCard.jsx
+++ components/Cards/PolicyCard/PolicyCard.jsx
@@ -1,26 +1,22 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import styles from './policyCard.module.css';
 import AppContext from "@/contexts/AppContext";
 
-
-export function PolicyMiniCard(props){
-  miniCardStyle=styles['policy-main-div'] +' '+hasScrollbar?'marginRight':"";
-  
-
+export function PolicyMiniCard(props) {
   const { hasScrollbar } = useContext(AppContext);
- 
-  return <div  className={props.bigTopMargin?miniCardStyle +' '+ styles['big-top-margin']:miniCardStyle}>
-  {props.children}
-  
-  </div>
+  const miniCardStyle = `${styles['policy-main-div']} ${hasScrollbar ? styles.marginRight : ''}`;
+
+  return (
+    <div className={props.bigTopMargin ? `${miniCardStyle} ${styles['big-top-margin']}` : miniCardStyle}>
+      {props.children}
+    </div>
+  );
 }
 
-export default function PolicyCard(props){
-
-    return <div className={props.smallContent?styles['policy-background-div']+  styles.bigBackDiv:styles['policy-background-div']}
-  > <PolicyMiniCard >{props.children}</PolicyMiniCard>
-  
+export default function PolicyCard(props) {
+  return (
+    <div className={props.smallContent ? `${styles['policy-background-div']} ${styles.bigBackDiv}` : styles['policy-background-div']}>
+      <PolicyMiniCard>{props.children}</PolicyMiniCard>
     </div>
-
-
+  );
 }
