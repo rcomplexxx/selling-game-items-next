bb1b305da6670872cbd8311d80a52cbe22dc4a9f
unknown <rcomplexx@gmail.com>
2023-05-26 22:19:05 +0200

    :bug fix

--- components/HomeReviews/HomeReviews.jsx
+++ components/HomeReviews/HomeReviews.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { RatingStar } from 'rating-star';
 import styles from './homeReviews.module.css';
 
@@ -8,23 +8,11 @@ const reviews = [
   { id: 3, title: "Thanks GameSmoke Gear3" }
 ];
 
-function Review({ title, style }) {
-  return (
-    <div className={styles.reviewDiv} style={style}>
-      <h1 className={styles.reviewTitle}>{title}</h1>
-      <RatingStar maxScore={5} id="123" rating={5} />
-      <p>
-        I sometimes don't like ordering online, but I decided to go with the flow. These products were out of this world! Can't believe it! Gosh! When it arrived, I gamed the whole night, and 3 hoes sucked my cock! Love it!
-      </p>
-      <h4>- Monika W.</h4>
-    </div>
-  );
-}
+
 
 export default function HomeReviews() {
   const [windowWidth, setWindowWidth] = useState(0);
   const [currentReview, setCurrentReview] = useState(0);
-  const reviewsRef = useRef(null);
 
   useEffect(() => {
     const handleResize = () => {
@@ -46,48 +34,58 @@ export default function HomeReviews() {
     setCurrentReview((prev) => (prev === reviews.length - 1 ? 0 : prev + 1));
   };
 
-  const handleSlide = () => {
-    const containerWidth = reviewsRef.current.offsetWidth;
-    const numReviews = reviews.length;
-    const reviewWidth = containerWidth / numReviews;
-    const maxScrollLeft = containerWidth - reviewWidth;
-
-    reviewsRef.current.scrollTo({
-      left: currentReview * reviewWidth,
-      behavior: "smooth",
-    });
-
-    if (reviewsRef.current.scrollLeft >= maxScrollLeft) {
-      setCurrentReview(0);
-    } else {
-      setCurrentReview((prev) => prev + 1);
-    }
-  };
-
   return (
     <>
       <h2 className={styles.title}>WHAT OUR CUSTOMERS HAVE TO SAY</h2>
-      <div className={styles.reviewsContainer} ref={reviewsRef}>
-        {reviews.map((review, index) => (
-          <Review
-            key={review.id}
-            title={review.title}
-            style={{
-              width: windowWidth > 1080 ? "340px" : "100%",
-            }}
-          />
-        ))}
-      </div>
-      {windowWidth <= 1080 && (
-        <div className={styles.arrowsContainer}>
+      <div className={styles.mainDiv}>
+        {windowWidth > 1080 ? (
+          reviews.map((review, index) => (
+            <Review
+              key={review.id}
+              title={review.title}
+              style={{
+                display: "flex" 
+              }}
+            />
+          ))
+        ) : (
+            <>
           <button className={`${styles.arrow} ${styles.left}`} onClick={prevReview}>
             &lt;
           </button>
-          <button className={`${styles.arrow} ${styles.right}`} onClick={nextReview}>
+          <Review
+            title={reviews[currentReview].title}
+            style={{
+              display: "flex",
+            }}
+          />
+        
+
+<button className={`${styles.arrow} ${styles.right}`} onClick={nextReview}>
             &gt;
           </button>
-        </div>
-      )}
+          
+          </>
+        )}
+
+
+
+      </div>
+      
     </>
+  );
+}
+
+
+function Review({ title, style }) {
+  return (
+    <div className={styles.reviewDiv} style={style}>
+      <h1 className={styles.reviewTitle}>{title}</h1>
+      <RatingStar maxScore={5} id="123" rating={5} />
+      <p>
+        I sometimes don't like ordering online, but I decided to go with the flow. These products were out of this world! Can't believe it! Gosh! When it arrived, I gamed the whole night, and 3 hoes sucked my cock! Love it!
+      </p>
+      <h4>- Monika W.</h4>
+    </div>
   );
 }
