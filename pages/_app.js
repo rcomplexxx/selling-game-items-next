0c9548cb02114149dcf716418d63b87e272a589b
unknown <rcomplexx@gmail.com>
2023-05-27 01:34:30 +0200

    :bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,56 +1,41 @@
-import '../styles/globals.css';
-import React, { useState, useEffect } from "react";
-import Navbar from "../components/Navbar/Navbar.jsx";
-import AppContext from '@/contexts/AppContext';
-import Footer from '@/components/Footer/Footer';
-// import { commerce } from "./lib/commerce";
-
 export default function App({ Component, pageProps }) {
-
-
   const [mobileOpen, setMobileOpen] = useState(false);
   const [cartProducts, setCartProducts] = useState([]);
   const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
   const [hasScrollbar, setHasScrollbar] = useState(false);
-  console.log('my Scroll'+hasScrollbar)
 
   useEffect(() => {
-    const hasVerticalScrollbar = window.visualViewport.width < document.body.clientWidth;
+    const hasVerticalScrollbar = window.innerWidth > document.documentElement.clientWidth;
     setHasScrollbar(hasVerticalScrollbar);
   }, []);
 
-  return (<>
-    <div
-      id="hronika"
-      style={{
-        
-        width: hasVerticalScrollbar?'calc(100%-10px)':'100%',
-        height: "fit-content",
-        display: "flex",
-        flexDirection: "column",
-        backgroundColor: "black",
-        color: "white",
-        minHeight: "100vh",
-        minWidth:"calc(100vw - 10px)",
-       alignItems:"center",
-      overflowY: 'auto'
-        
-      }}
-    >
-    
- 
+  return (
+    <>
+      <div
+        id="hronika"
+        style={{
+          width: hasScrollbar ? 'calc(100% - 10px)' : '100%',
+          height: 'fit-content',
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: 'black',
+          color: 'white',
+          minHeight: '100vh',
+          minWidth: 'calc(100vw - 10px)',
+          alignItems: 'center',
+          overflowY: 'auto'
+        }}
+      >
         <Navbar
           totalItems={cartProducts.length}
           handleDrawerToggle={handleDrawerToggle}
         />
 
         <AppContext.Provider value={{ cartProducts, setCartProducts }}>
-       
           <Component {...pageProps} />
-         
         </AppContext.Provider>
 
-        <Footer/>
+        <Footer />
       </div>
     </>
   );
