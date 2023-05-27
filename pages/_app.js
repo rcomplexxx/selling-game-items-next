684b558fed48ddb6650f293669e0d78eadc3971b
unknown <rcomplexx@gmail.com>
2023-05-27 06:56:01 +0200

    :bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -31,7 +31,7 @@ export default function App({ Component, pageProps }) {
       <div
         id="hronika"
         style={{
-          width: hasVerticalScrollbar?'calc(100% - 10px)':'100%',
+          width: hasScrollbar?'calc(100% - 10px)':'100%',
           height: 'fit-content',
           display: 'flex',
           flexDirection: 'column',
