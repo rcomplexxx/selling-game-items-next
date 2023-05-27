47264e5324bc8eb495c72d3d15201eb2dc2c6615
unknown <rcomplexx@gmail.com>
2023-05-27 06:22:51 +0200

    :bug fix

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,10 @@ import HomeReviews from '../components/HomeReviews/HomeReviews.jsx'
 import Products from '@/components/Products/Products.jsx';
 import products from "../data/products.json";
 import styles from "../styles/appStyle.module.css";
-import Image from 'next/image.js';
 const HomePage = ({products}) => {
   return ( <>
      
      <div className={styles.heroImageDiv}></div>
-     <Image className={styles.heroImage}
-      src={'/images/gamerwp.png'} // Path to your image from the `public` directory
-      alt="Example Image"
-    
-      objectFit="cover"
-      fill
-     
-     ></Image>
                 <div className={styles.heroWindow}>
                   <button className={styles.linkButton +' '+ styles.linkButtonFirst}>Shop Now</button>
                   <button className={styles.linkButton}>New Items</button>
