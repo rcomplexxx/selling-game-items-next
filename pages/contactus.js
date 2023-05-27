19ede4a239bc901d04cea16885685f8a8b7c99cb
unknown <rcomplexx@gmail.com>
2023-05-27 02:10:54 +0200

    :bug fix

--- pages/contactus.js
+++ pages/contactus.js
@@ -7,11 +7,10 @@ export default function ContactUs(){
 
 
 
-return <div className="centered"><PolicyCard  smallContent={true}>
+return <PolicyCard  smallContent={true}>
     <h1>Contact Us</h1>
 <p>Drop Us A Line</p>
 <p>For any questions regarding our product or for assistance with your order, please email us at support@petboutique.co and you should get a response within one business day. If you are emailing about your order, please make sure to include the order number or the email address used at the checkout in your message. Thank you!</p>
 </PolicyCard>
-</div>
 
 }
