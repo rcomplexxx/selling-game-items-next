42a93767d63801df9971d696cd9846b4cd718346
unknown <rcomplexx@gmail.com>
2023-06-04 11:07:54 +0200

    :bug fixed

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -8,7 +8,8 @@ export default function Footer() {
 
     <h1>Stay connected</h1>
     <p>Join our newsletters to get the latest SmokeGamerItems updates and surprise discounts!</p>
-
+    <input>Enter your email adress</input>
+    <button>Subscribe</button>
       <div className={styles.footerLinks}>
       <Link href="/" className={styles.footerLink}>Home</Link>
         <Link href="/products" className={styles.footerLink}>Shop</Link>
