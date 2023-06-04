0be4ad7bb1439b5b84050de42c210041a59d29a9
unknown <rcomplexx@gmail.com>
2023-06-04 02:28:13 +0200

    :bug fixed

--- components/HomeReviews/HomeReviews.jsx
+++ components/HomeReviews/HomeReviews.jsx
@@ -64,7 +64,7 @@ export default function HomeReviews() {
           ))
         ) : (
           <>
-          <div className={styles.arrowContainer}>
+          <div className={styles.arrowsContainer}>
   <button className={styles.arrow} onClick={prevReview}>
     <FontAwesomeIcon icon={faArrowLeft} />
   </button>
