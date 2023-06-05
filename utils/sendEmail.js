60d08edb60826a74f648675d124b4a665fdd2bd7
unknown <rcomplexx@gmail.com>
2023-06-05 14:45:02 +0200

    :bug fixed

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer'
 // Create a transporter using your environment variables
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+  port: parseInt(process.env.EMAIL_PORT),
   secure: process.env.EMAIL_SECURE === 'true',
   auth: {
     user: process.env.EMAIL_USER,
