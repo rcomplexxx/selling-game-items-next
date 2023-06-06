c97f00f71a14a2a76cbb129e171259ccf24a7dec
unknown <rcomplexx@gmail.com>
2023-06-06 05:18:18 +0200

    :bug fixed

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -40,7 +40,8 @@ const NavBar = ({ totalItems }) => {
 
 
       <MenuItem style={{display:'flex', justifyContent:"flex-end"}}>
-        <button style={{border:'none', color:"var(--red-2)", backgroundColor:"transparent", marginLeft:"10px", marginRight:"10px",fontWeight:"400"}} onClick={handleMobileMenuClose}>
+        <button style={{border:'none', color:"var(--red-2)", backgroundColor:"transparent", marginLeft:"10px", marginRight:"10px",
+        fontSize:"16px",fontWeight:"400"}} onClick={handleMobileMenuClose}>
           <Typography variant="body1">x</Typography>
         </button>
       </MenuItem>
