f93f1485b8dff561f83a7a7d468b4663fbad94d9
unknown <rcomplexx@gmail.com>
2023-06-06 10:06:54 +0200

    :bug fixed

--- components/Cart/Cart.jsx
+++ components/Cart/Cart.jsx
@@ -59,11 +59,11 @@ const Cart = ({    emptyCartText='true' }) => {
   );
 
   return (
-    <div className="container">
+    <Container>
       <div className={styles.toolbar} />
       {(emptyCartText || cartProducts.length!==0) && <Typography className={classNames(styles.title)} variant="h3" gutterBottom>Your Shopping Cart</Typography>}
       { cartProducts.length==0 ? renderEmptyCart() : renderCart() }
-    </div>
+    </Container>
   );
 };
 
