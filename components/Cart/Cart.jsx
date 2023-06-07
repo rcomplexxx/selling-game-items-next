d5feb9b6f8c900bcea8dc7cf24e12521f2f3ae74
unknown <rcomplexx@gmail.com>
2023-06-07 10:13:06 +0200

    :bug fixed

--- components/Cart/Cart.jsx
+++ components/Cart/Cart.jsx
@@ -47,7 +47,7 @@ const Cart = ({    emptyCartText='true' }) => {
               sm={6}
               md={4}
               lg={3}
-              key={lineItem.id} sx={{width:"100%"}}>
+              key={lineItem.id} >
             <CartItem item={lineItem}  />
           </Grid>
         ))}
