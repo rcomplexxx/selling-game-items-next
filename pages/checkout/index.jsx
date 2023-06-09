273aa0aa5d89e6312d73797c7d5670696862d400
unknown <rcomplexx@gmail.com>
2023-06-09 16:43:51 +0200

    :bug fixed

--- pages/checkout/index.jsx
+++ pages/checkout/index.jsx
@@ -2,6 +2,73 @@ import React from 'react';
 import styles from './checkout.module.css';
 
 const CheckoutPage = () => {
+
+
+  const orderDetails = ( <div className={styles.checkout_right}>
+     
+
+    <div className={styles.checkout_section}>
+      <h2>Order Summary</h2>
+      <div className={styles.order_summary}>
+        <div className={styles.product}>
+          <p>3 Fairy Light Spirit Tree</p>
+          <p>$119.97 USD</p>
+        </div>
+        <div className={styles.coupon_code}>
+          <input type="text" placeholder="Coupon code" />
+        </div>
+        <div className={styles.subtotal}>
+          <p>Subtotal</p>
+          <p>$119.97 USD</p>
+        </div>
+        <div className={styles.shipping}>
+          <p>Shipping</p>
+          <p>Free</p>
+        </div>
+        <div className={styles.tax}>
+          <p>Tax</p>
+          <p>$0.00</p>
+        </div>
+        <div className={styles.total}>
+          <p>Total</p>
+          <p>$119.97 USD</p>
+        </div>
+        <div className={styles.savings}>
+          <p>You're Saving</p>
+          <p>$120.00</p>
+        </div>
+      </div>
+      <button className={styles.checkout_button}>Checkout</button>
+      </div>
+
+      <div className={styles.checkout_section}>
+      <h2>What Happy Customers Are Saying</h2>
+      <div className={styles.customer_review}>
+        <div className={styles.review}>
+          <p>Selina J., Dallas, TX</p>
+          <p>Verified Buyer</p>
+          <p>★★★★★ 5/5</p>
+          <p>After trying the Fairy Light Spirit Tree, I ordered 3 more! It transformed my entire home.</p>
+        </div>
+        <div className={styles.review}>
+          <p>Sarah H., London, UK</p>
+          <p>Verified Buyer</p>
+          <p>★★★★★ 5/5</p>
+          <p>I don’t miss shopping in stores at all. This is as easy as it comes. Check out online and receive your Lights at your doorstep! Fast delivery and when I made a mistake, I just exchanged it. They’re that good.</p>
+        </div>
+        <div className={styles.review}>
+          <p>Amy P., Salt Lake City, UT</p>
+          <p>Verified Buyer</p>
+          <p>★★★★★ 5/5</p>
+          <p>This was my first time buying from a Facebook advertisement, and customer service came to the rescue. 10/10 service!</p>
+        </div>
+      </div>
+    </div>
+
+    </div>);
+
+
+
   return (
     <div className={styles.checkout_container}>
         <div className={styles.checkout_left}>
@@ -92,70 +159,9 @@ const CheckoutPage = () => {
       </div>
 
       </div>
-      <div className={styles.checkout_right}>
      
 
-      <div className={styles.checkout_section}>
-        <h2>Order Summary</h2>
-        <div className={styles.order_summary}>
-          <div className={styles.product}>
-            <p>3 Fairy Light Spirit Tree</p>
-            <p>$119.97 USD</p>
-          </div>
-          <div className={styles.coupon_code}>
-            <input type="text" placeholder="Coupon code" />
-          </div>
-          <div className={styles.subtotal}>
-            <p>Subtotal</p>
-            <p>$119.97 USD</p>
-          </div>
-          <div className={styles.shipping}>
-            <p>Shipping</p>
-            <p>Free</p>
-          </div>
-          <div className={styles.tax}>
-            <p>Tax</p>
-            <p>$0.00</p>
-          </div>
-          <div className={styles.total}>
-            <p>Total</p>
-            <p>$119.97 USD</p>
-          </div>
-          <div className={styles.savings}>
-            <p>You're Saving</p>
-            <p>$120.00</p>
-          </div>
-        </div>
-        <button className={styles.checkout_button}>Checkout</button>
-        </div>
-
-        <div className={styles.checkout_section}>
-        <h2>What Happy Customers Are Saying</h2>
-        <div className={styles.customer_review}>
-          <div className={styles.review}>
-            <p>Selina J., Dallas, TX</p>
-            <p>Verified Buyer</p>
-            <p>★★★★★ 5/5</p>
-            <p>After trying the Fairy Light Spirit Tree, I ordered 3 more! It transformed my entire home.</p>
-          </div>
-          <div className={styles.review}>
-            <p>Sarah H., London, UK</p>
-            <p>Verified Buyer</p>
-            <p>★★★★★ 5/5</p>
-            <p>I don’t miss shopping in stores at all. This is as easy as it comes. Check out online and receive your Lights at your doorstep! Fast delivery and when I made a mistake, I just exchanged it. They’re that good.</p>
-          </div>
-          <div className={styles.review}>
-            <p>Amy P., Salt Lake City, UT</p>
-            <p>Verified Buyer</p>
-            <p>★★★★★ 5/5</p>
-            <p>This was my first time buying from a Facebook advertisement, and customer service came to the rescue. 10/10 service!</p>
-          </div>
-        </div>
-      </div>
-
-      </div>
-
-      
+      {orderDetails}
     </div>
   );
 };
