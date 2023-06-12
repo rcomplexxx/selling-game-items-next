3b9bf96d3d6c0845d8e50134ff84d5a77309d911
unknown <rcomplexx@gmail.com>
2023-06-12 14:14:39 +0200

    :bug fixed

--- components/Checkout/CheckoutInfo.jsx
+++ components/Checkout/CheckoutInfo.jsx
@@ -1,67 +1,117 @@
-import React from "react";
-import styles from './ckechoutinfo.module.css'
+import React, { useState } from "react";
+import styles from './checkoutinfo.module.css';
 
+export default function CheckoutInfo() {
+  const [errors, setErrors] = useState({});
 
-export default function CheckoutInfo(){
+  const handleBlur = (event) => {
+    const { id, value } = event.target;
 
+    if (!value) {
+      setErrors((prevErrors) => ({ ...prevErrors, [id]: "This field is empty." }));
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, [id]: "" }));
+    }
+  };
 
-
-    return  <div className={styles.checkout_left}>
-    <div className={styles.checkout_section}>
-      <h2>Contact Information</h2>
-      <form>
-        <div className={styles.form_group}>
-          <label htmlFor="email">Email *</label>
-          <input type="email" id="email" />
-        </div>
-        <div className={styles.form_group}>
-          <label htmlFor="emailOffers">Email me with news and offers</label>
-          <input type="checkbox" id="emailOffers" />
-        </div>
-      </form>
-    </div>
-
-    <div className={styles.checkout_section}>
-      <h2>Shipping Address</h2>
-      <form>
-        <div className={styles.form_group}>
-          <label htmlFor="firstName">First name *</label>
-          <input type="text" id="firstName" />
-        </div>
-        <div className={styles.form_group}>
-          <label htmlFor="lastName">Last name *</label>
-          <input type="text" id="lastName" />
-        </div>
-        <div className={styles.form_group}>
-          <label htmlFor="address">Address *</label>
-          <input type="text" id="address" />
-        </div>
-        <div className={styles.form_group}>
-          <label htmlFor="apt">Apt, suite, etc. (optional)</label>
-          <input type="text" id="apt" />
-        </div>
-        <div className={styles.form_group}>
-          <label htmlFor="country">Country *</label>
-          <input type="text" id="country" />
-        </div>
-        <div className={styles.form_group}>
-          <label htmlFor="postcode">Postcode *</label>
-          <input type="text" id="postcode" />
-        </div>
-        <div className={styles.form_group}>
-          <label htmlFor="state">State *</label>
-          <input type="text" id="state" />
-        </div>
-        <div className={styles.form_group}>
-          <label htmlFor="suburb">Suburb *</label>
-          <input type="text" id="suburb" />
-        </div>
-        <div className={styles.form_group}>
-          <label htmlFor="phone">Phone *</label>
-          <input type="text" id="phone" />
-        </div>
-      </form>
-    </div>
+  return (
+    <div className={styles.checkout_left}>
+      <div className={styles.checkout_section}>
+        <h2>Contact Information</h2>
+        <form>
+          <div className={styles.form_group}>
+            <label htmlFor="email">Email *</label>
+            <input
+              type="email"
+              id="email"
+              onBlur={handleBlur}
+            />
+            {errors.email && <p className={styles.error}>{errors.email}</p>}
+          </div>
+          <div className={styles.form_group}>
+            <label htmlFor="firstName">First name *</label>
+            <input
+              type="text"
+              id="firstName"
+              onBlur={handleBlur}
+            />
+            {errors.firstName && <p className={styles.error}>{errors.firstName}</p>}
+          </div>
+          <div className={styles.form_group}>
+            <label htmlFor="lastName">Last name *</label>
+            <input
+              type="text"
+              id="lastName"
+              onBlur={handleBlur}
+            />
+            {errors.lastName && <p className={styles.error}>{errors.lastName}</p>}
+          </div>
+          <div className={styles.form_group}>
+            <label htmlFor="address">Address *</label>
+            <input
+              type="text"
+              id="address"
+              onBlur={handleBlur}
+            />
+            {errors.address && <p className={styles.error}>{errors.address}</p>}
+          </div>
+          <div className={styles.form_group}>
+            <label htmlFor="apt">Apt, suite, etc. (optional)</label>
+            <input
+              type="text"
+              id="apt"
+              onBlur={handleBlur}
+            />
+            {errors.apt && <p className={styles.error}>{errors.apt}</p>}
+          </div>
+          <div className={styles.form_group}>
+            <label htmlFor="country">Country *</label>
+            <input
+              type="text"
+              id="country"
+              onBlur={handleBlur}
+            />
+            {errors.country && <p className={styles.error}>{errors.country}</p>}
+          </div>
+          <div className={styles.form_group}>
+            <label htmlFor="postcode">Postcode *</label>
+            <input
+              type="text"
+              id="postcode"
+              onBlur={handleBlur}
+            />
+            {errors.postcode && <p className={styles.error}>{errors.postcode}</p>}
+          </div>
+          <div className={styles.form_group}>
+            <label htmlFor="state">State *</label>
+            <input
+              type="text"
+              id="state"
+              onBlur={handleBlur}
+            />
+            {errors.state && <p className={styles.error}>{errors.state}</p>}
+          </div>
+          <div className={styles.form_group}>
+            <label htmlFor="suburb">Suburb *</label>
+            <input
+              type="text"
+              id="suburb"
+              onBlur={handleBlur}
+            />
+            {errors.suburb && <p className={styles.error}>{errors.suburb}</p>}
+          </div>
+          <div className={styles.form_group}>
+            <label htmlFor="phone">Phone *</label>
+            <input
+              type="text"
+              id="phone"
+              onBlur={handleBlur}
+            />
+            {errors.phone && <p className={styles.error}>{errors.phone}</p>}
+          </div>
+        </form>
+      </div>
+ 
 
     <div className={styles.checkout_section}>
       <h2>Billing Address</h2>
