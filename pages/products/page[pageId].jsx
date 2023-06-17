4e3e035efc7b9d06352aa6f1b80950b474aeec0d
unknown <rcomplexx@gmail.com>
2023-06-17 19:47:19 +0200

    :bug fixed

--- pages/products/page[pageId].jsx
+++ pages/products/page[pageId].jsx
@@ -1,18 +1,6 @@
 import React from "react";
-import { useRouter } from "next/router";
-import Products from "../../components/Products/Products.jsx";
 
 const ProductPage = () => {
-  const router = useRouter();
-  const { pageId } = router.query;
-
-  // Assuming you have the products array available
-  const products = [
-    // Your products data here
-  ];
-
-  const currentPage = parseInt(pageId);
-
   return <p>This is Product Page</p>;
 };
 
