17a24e8fb0966c0a3975cacc3597a4b34dbd3636
unknown <rcomplexx@gmail.com>
2023-06-17 18:18:32 +0200

    :bug fixed

--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -35,43 +35,35 @@ const Products = ({ products, showAll }) => {
     setCartProducts(newCartProducts);
   };
 
+  const renderProducts = (start, end) => {
+    return products
+      .slice(start, end)
+      .map((product) => (
+        <Grid
+          className={styles.productGridStyle}
+          key={product.id}
+          item
+          xs={12}
+          sm={6}
+          md={4}
+          lg={3}
+        >
+          <Product key={product.id} product={product} onAddToCart={onAddToCart} />
+        </Grid>
+      ));
+  };
 
   const renderAll = () => {
-    return products.map((product) => (
-      <Grid
-        className={styles.productGridStyle}
-        key={product.id}
-        item
-        xs={12}
-        sm={6}
-        md={4}
-        lg={3}
-      >
-        <Product key={product.id} product={product} onAddToCart={onAddToCart} />
-      </Grid>
-    ));
+    return renderProducts(0, products.length);
   };
 
-  const renderSome= ()=>{
-    return products
-    .slice(0, 6) // Slice the products array to get only the first 9 products
-    .map((product, i) => (
-      <Grid
-        className={styles.productGridStyle}
-        key={product.id}
-        item
-        xs={12}
-        sm={6}
-        md={4}
-        lg={4}
-      >
-        <Product key={product.id} product={product} onAddToCart={onAddToCart} />
-      </Grid>
-    ));
+  const renderPage = (page) => {
+    const start = (page - 1) * 16;
+    const end = start + 16;
+    return renderProducts(start, end);
   };
 
- 
-  
+  const totalPages = Math.ceil(products.length / 16);
 
   return (
     <div className={styles.toolbar}>
@@ -82,10 +74,31 @@ const Products = ({ products, showAll }) => {
           container
           justifyContent="flex-start"
         >
-          {showAll?renderAll():renderSome()}
-        </Grid>    
+          {showAll ? renderAll() : renderPage(1)}
+        </Grid>
       </div>
-      {!showAll && <Link href="/products" style={{ marginTop:"10px", marginBottom:"20px", fontSize:"28px", padding:"8px 16px", color:"gray", backgroundColor:"transparent", border:"solid gray 1px", textDecoration:"none" }}>View All</Link>}
+      {!showAll && (
+        <div>
+          {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+            <Link
+              key={page}
+              href={`/products?page=${page}`}
+              style={{
+                marginTop: "10px",
+                marginBottom: "20px",
+                fontSize: "28px",
+                padding: "8px 16px",
+                color: "gray",
+                backgroundColor: "transparent",
+                border: "solid gray 1px",
+                textDecoration: "none",
+              }}
+            >
+              Page {page}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
