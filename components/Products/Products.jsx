b9a1ffe15b725d77df32beea28d67daa1d681da9
unknown <rcomplexx@gmail.com>
2023-06-21 23:54:21 +0200

    :bug fixed

--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -6,34 +6,7 @@ import styles from "./products.module.css";
 import AppContext from "@/contexts/AppContext";
 import Link from "next/link";
 
-const Products = ({ products, showAll, lastPage=false }) => {
-
-  console.log(lastPage);
-
-
-  const [screenSize, setScreenSize] = useState(null);
-
-  useEffect(() => {
-    if(lastPage){const handleResize = () => {
-        if(window.innerWidth < 600)  setScreenSize(1);
-        else if(window.innerWidth < 960)  setScreenSize(2);
-        else if(window.innerWidth < 1280)  setScreenSize(3);
-        else  setScreenSize(3);
-
-      setIsSmallScreen(window.innerWidth < 960);
-    };
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
- 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }
-  }, []);
-
-
+const Products = ({ products, showAll }) => {
   const { cartProducts, setCartProducts } = useContext(AppContext);
 
   const onAddToCart = async (product, quantity = 1) => {
@@ -76,19 +49,17 @@ const Products = ({ products, showAll, lastPage=false }) => {
         <Grid
           className={styles.mainGridStyle}
           container
-          justifyContent="center"
+          justifyContent="flex-start"
         >
       {products.map((product, i) => (
       <Grid
         className={styles.productGridStyle}
-        style={(lastPage&&products.length-i<screenSize) && {justifyContent:"flex-start"}}
         key={product.id}
         item
         xs={12}
         sm={6}
         md={4}
         lg={4}
-       
       >
         <Product key={product.id} product={product} onAddToCart={onAddToCart} />
       </Grid>))
