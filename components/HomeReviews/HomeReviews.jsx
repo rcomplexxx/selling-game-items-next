157b5069cd80788db512a607f5c8569d54d9ab29
unknown <rcomplexx@gmail.com>
2023-06-21 05:55:24 +0200

    :bug fixed

--- components/HomeReviews/HomeReviews.jsx
+++ components/HomeReviews/HomeReviews.jsx
@@ -1,3 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { RatingStar } from 'rating-star';
+import styles from './homeReviews.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay, virtualize } from 'react-swipeable-views-utils';
+import Pagination from './Pagination';
+
+const reviews = [
+  { id: 1, title: "Review one", reviewText: "I sometimes don't like ordering online, but I decided to go with the flow. These products were out of this world! Can't believe it! Gosh! When it arrived, I gamed the whole night, and had a perfect gaming night! Love it!",
+  author: 'Monika W.'  },
+  { id: 2, title: "Review two", reviewText: "I love the headphones I got from here. The sound is three dimensional, litelarry!" ,
+  author: 'Marta N.' },
+  { id: 3, title: "Review three", 
+  reviewText: "I knew I found the best online store for gaming when I've seen amazing keyboard that I couldn't find anywhere! I ordered, it arrived, and I am more then satisfied with product." ,
+  author: 'Luke B.' 
+  },
+];
+
+const AutoPlaySwipeableViews = autoPlay(virtualize(SwipeableViews));
+
+function Review({ title, reviewText, author, style, smallScreen=false }) {
+
+  return (
+    <div className={styles.reviewDiv} style={style}>
+      <h1 className={styles.reviewTitle}>{title}</h1>
+      <RatingStar maxScore={5} id="123" rating={5} />
+      <p>{reviewText}</p>
+      <h4>- {author}</h4>
+    </div>
+  );
+}
+
 export default function HomeReviews() {
   const [windowWidth, setWindowWidth] = useState(0);
   const [currentReview, setCurrentReview] = useState(0);
