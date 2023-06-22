092d2ddd738c4793c54859b38ddb1895d39a5482
unknown <rcomplexx@gmail.com>
2023-06-23 01:07:08 +0200

    :bug fixed

---
+++ components/CustomerReviews/CustomerReviews.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Image from "next/image";
+
+export default function CustomerReviews(){
+
+
+
+
+
+
+
+    return <div className="centered">
+        <h1>Customer Reviews</h1>
+
+         <div style={{ width: '200px', maxHeight: 'max-content', backgroundColor:"gray" }}>
+    <div style={{ position: 'relative', width: '100%', height:"max-content"}}>
+      <Image
+        src="/images/1.png"
+        alt="Product Image"
+        layout="fill"
+        objectFit="contain"
+      />
+    </div>
+  </div>
+
+
+    </div>
+}
