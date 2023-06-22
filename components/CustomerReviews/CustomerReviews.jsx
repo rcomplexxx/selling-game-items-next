21de736730e11b0a8379e37d9b6dd9db4263c767
unknown <rcomplexx@gmail.com>
2023-06-23 00:47:15 +0200

    :bug fixed

--- components/CustomerReviews/CustomerReviews.jsx
+++ components/CustomerReviews/CustomerReviews.jsx
@@ -11,6 +11,17 @@ export default function CustomerReviews(){
     return <div className="centered">
         <h1>Customer Reviews</h1>
 
+         <div style={{ width: '200px', maxHeight: 'max-content' }}>
+    <div style={{ position: 'relative', width: '100%', paddingBottom: '100%' }}>
+      <Image
+        src="/images/1.png"
+        alt="Product Image"
+        layout="fill"
+        objectFit="contain"
+      />
+    </div>
+  </div>
+
 
     </div>
 }
