05b0e2489a72515562e8206270578858c5d553c0
unknown <rcomplexx@gmail.com>
2023-06-23 01:24:13 +0200

    :bug fixed

--- components/CustomerReviews/CustomerReviews.jsx
+++ components/CustomerReviews/CustomerReviews.jsx
@@ -18,8 +18,10 @@ export default function CustomerReviews(){
   width={0}
   height={0}
   sizes="100vw"
-  style={{ width: '100%', height: 'auto' }} // optional
+  style={{ width: '100%', height: 'auto', marginBottom:"5px" }} // optional
 />
+LLLLL
+<p style={{marginTop:"5px"}}>Very nice and very happy. Def need to be creative to hang it and would be nice if it came with hooks but all in all it’s very nice. Good accent light for a baby room!</p>
   </div>
 
 
