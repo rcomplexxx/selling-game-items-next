4089e6806526bcb3fc0bf7b60cb0c93ebbf2a1a6
unknown <rcomplexx@gmail.com>
2023-06-24 02:56:37 +0200

    :bug fixed

--- components/CustomerReviews/CustomerReviews.jsx
+++ components/CustomerReviews/CustomerReviews.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { Grid } from "@mui/material";
 import styles from './customerreviews.module.css'
+import { RatingStar } from 'rating-star';
 
 
 function Review(){
@@ -15,7 +16,7 @@ height={0}
 sizes="100vw"
 style={{ width: '100%', height: 'auto', marginBottom:"5px" }} // optional
 />
-LLLLL
+<RatingStar maxScore={5} id="123" rating={5} />
 <p style={{marginTop:"5px"}}>Very nice and very happy. Def need to be creative to hang it and would be nice if it came with hooks but all in all it’s very nice. Good accent light for a baby room!</p>
 </div></>
 
