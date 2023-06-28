f718c1a0da51a31ef093822ae3be2a714ef67323
unknown <rcomplexx@gmail.com>
2023-06-28 03:00:03 +0200

    :optimization bug fixed

--- pages/_app.js
+++ pages/_app.js
@@ -40,7 +40,7 @@ export default function App({ Component, pageProps }) {
           <Component {...pageProps} />
         </AppContext.Provider>
 
-        {router.pathname!=='checkout' && <Footer />}
+        {router.pathname!=='/checkout' && <Footer />}
       </div>
   );
 }
