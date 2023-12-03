dc0b4ccadc521f3a0ab268e7a377bfa0c0728990
rcomplexxx <rcomplexx@gmail.com>
2023-12-03 14:01:13 +0100

    :design improved

--- components/ProductPics/Thumbnails.jsx
+++ components/ProductPics/Thumbnails.jsx
@@ -23,7 +23,7 @@ export default function Thumbnails({ images, imageIndex, swiper, setImageIndex, 
     <div className={styles.slider2Suiter}>
    
 
-   <div className={`${styles.leftArrowDiv} ${imageIndex===0 && styles.disabledArray}`}
+   <div className={`${styles.leftArrowDiv} ${imageIndex===0 && styles.disabledArrow}`}
    
    onClick={()=>{ swiper.slideTo(imageIndex-1);}}>
                 <Image 
@@ -35,7 +35,7 @@ export default function Thumbnails({ images, imageIndex, swiper, setImageIndex, 
                 />
             </div>
 
-            <div className={`${styles.leftArrowDiv} ${styles.rightArrowDiv} ${imageIndex===images.length-1 && styles.disabledArray}`} onClick={()=>{ swiper.slideTo(imageIndex+1);}}>
+            <div className={`${styles.leftArrowDiv} ${styles.rightArrowDiv} ${imageIndex===images.length-1 && styles.disabledArrow}`} onClick={()=>{ swiper.slideTo(imageIndex+1);}}>
                 <Image 
                 height={0}
                 width={0}
