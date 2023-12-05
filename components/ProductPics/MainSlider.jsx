1e86b6e95bceb20445458419a1f3586c24e58855
rcomplexxx <rcomplexx@gmail.com>
2023-12-05 10:40:03 +0100

    :optimization

--- components/ProductPics/MainSlider.jsx
+++ components/ProductPics/MainSlider.jsx
@@ -17,7 +17,6 @@ export default function MainSlider({setZoomed, mobileInterface, images, imageInd
     
     centeredSlides: false,
 
-    className: styles.myslider,
    
    
     onSlideChange: (swiper) => {
