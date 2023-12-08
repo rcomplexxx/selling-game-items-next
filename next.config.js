7a877baac0d3ad4faab9f9c53dafedd1fff923bd
rcomplexxx <rcomplexx@gmail.com>
2023-12-08 17:22:04 +0100

    :optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    experimental: {
+        scrollRestoration: true,
+      },
+      compression: true,
 }
 
 module.exports = nextConfig
