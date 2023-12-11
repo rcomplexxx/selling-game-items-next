e1240206158c21e8c188442e690d4ecbc58b963a
rcomplexxx <rcomplexx@gmail.com>
2023-12-11 17:26:09 +0100

    :zoom improved

--- components/ProductPics/FullScreenZoomableImages/FullScreenZoomableImages.jsx
+++ components/ProductPics/FullScreenZoomableImages/FullScreenZoomableImages.jsx
@@ -2,7 +2,10 @@ import React, { useRef, useEffect, useState } from "react";
 import styles from "./fullscreenzoomableimage.module.css";
 import { Zoom, Navigation, Pagination } from 'swiper/core';
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import 'swiper/swiper-bundle.css';
+
+// Import Swiper styles
+import 'swiper/swiper.min.css';
 import 'swiper/css/zoom'
 import Image from "next/image";
 
@@ -16,7 +19,18 @@ const FullScreenZoomableImage = ({ imageIndex, fullScreenChange, images }) => {
     <>
       <div className={styles.full_screen_container}>
         <div className={styles.spaceController}>
-        
+          <div className={styles.closeSuiter}>
+            <Image
+            height={0}
+            width={0}
+            sizes="24px"
+              src="/images/cancelWhite.png"
+              onClick={() => {
+                fullScreenChange(imageIndex);
+              }}
+              className={styles.close_button}
+            />
+          </div>
 
           <Swiper
           speed={400}
@@ -29,7 +43,7 @@ const FullScreenZoomableImage = ({ imageIndex, fullScreenChange, images }) => {
             className={styles.productImageSwiper}
           >
             {images.map((image, index) => (
-              <SwiperSlide key={index} className={`carousel-item`}>
+              <SwiperSlide key={index}>
                   <div className="swiper-zoom-container">
                 <div id="zoomDiv" className={`${styles.productImageDiv} swiper-zoom-target`}>
               
