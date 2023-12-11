b1f7333b7e5609d293c57aa2a84f7663a8298e47
rcomplexxx <rcomplexx@gmail.com>
2023-12-11 17:05:13 +0100

    :zoom improved

--- components/ProductPics/FullScreenZoomableImages/FullScreenZoomableImages.jsx
+++ components/ProductPics/FullScreenZoomableImages/FullScreenZoomableImages.jsx
@@ -33,15 +33,15 @@ const FullScreenZoomableImage = ({ imageIndex, fullScreenChange, images }) => {
           speed={400}
          slidesPerView={1}
            
-            zoom={true}
+           
             initialSlide={imageIndex}
             onSwiper={setSwiper}
-            modules={[Zoom]}
+            
             className={styles.productImageSwiper}
           >
             {images.map((image, index) => (
               <SwiperSlide key={index} className={`carousel-item`}>
-                  <div className="swiper-zoom-container">
+                 
                 <div id="zoomDiv" className={`${styles.productImageDiv} swiper-zoom-target`}>
               
                   <Image
@@ -53,7 +53,7 @@ const FullScreenZoomableImage = ({ imageIndex, fullScreenChange, images }) => {
                     className={`${styles.productImage}`}
                    
                   />
-                </div>  </div>
+                </div>  
                
               </SwiperSlide>
             ))}
