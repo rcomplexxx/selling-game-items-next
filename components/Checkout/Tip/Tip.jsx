05acafcb4da4a394fa4fdc610768f47335d2e496
rcomplexxx <rcomplexx@gmail.com>
2024-01-23 11:02:35 +0100

    :checkout improved

--- components/Checkout/Tip/Tip.jsx
+++ components/Checkout/Tip/Tip.jsx
@@ -1,17 +1,19 @@
 
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import InputField from '../Input/InputField'
 import styles from './tip.module.css'
 
 
 export default function Tip({products, tip, setTip}){
 
-    const [tipFieldOpen, setTipFieldOpen] = useState(false);
+    const [tipShow, setTipShow] = useState(false);
     const [selectedField, setSelectedField] =useState();
     const [fullProductCost, setFullProductCost] = useState(0);
     
     const [tipInputValue, setTipInputValue]= useState("");
     const [tipInputFocused, setTipInputFocused] = useState(false);
+    const [tipError, setTipError] = useState();
+    const visibilityTimeout = useRef();
 
 
     useEffect(() => {
@@ -27,25 +29,53 @@ export default function Tip({products, tip, setTip}){
           setFullProductCost(fullProductCostNow);
       }, [products]);
 
+
+  useEffect(()=>{
+
+          clearTimeout(visibilityTimeout.current);
+          const tipDiv= document.getElementById("tipDiv")
+          if(tipShow){
+         
+            tipDiv.style.maxHeight=`${ tipDiv.scrollHeight}px`;
+          visibilityTimeout.current=setTimeout(()=>{
+            tipDiv.style.overflow = `visible`
+           }, 600)
+          }
+          else{
+            tipDiv.style.overflow = `hidden`
+            tipDiv.style.maxHeight=`0`;
+          }
+        },[tipShow])
       
 
 
     return <div className={styles.tipMainDiv}>
-       
+
+
+<div className={styles.tipShowCheckboxDiv}  onClick={()=>{setTipShow(!tipShow)}}>
+      <input type="checkbox" className={styles.tipShowChecker} checked={tipShow}
+     
+      />
+      <label className={styles.tipShowCheckboxLabel}>
+     Show your support for the GameBuff team
+    </label>
+    </div>
+      
+      <div id='tipDiv' className={styles.tipDiv}>
         <div className={styles.roundPercentWrapper}>
             <div className={`${styles.roundPercent} ${selectedField==5 && styles.selectedPercent}`} 
             onClick={()=>{
-               setTipInputValue(""); setSelectedField(5); setTip((fullProductCost*5/100).toFixed(2));}}>
+              setTipError(); setTipInputValue(""); setSelectedField(5); setTip((fullProductCost*5/100).toFixed(2));}}>
             <span className={styles.roundPercentSpan}>5%</span>
             <span className={styles.roundTipSpan}>${(fullProductCost*5/100).toFixed(2)}</span>
             </div>
             <div className={`${styles.roundPercent} ${selectedField==10 && styles.selectedPercent}`}
-             onClick={()=>{setTipInputValue(""); setSelectedField(10); setTip((fullProductCost*10/100).toFixed(2));}}>
+             onClick={()=>{setTipError();setTipInputValue(""); setSelectedField(10); setTip((fullProductCost*10/100).toFixed(2));}}>
             <span className={styles.roundPercentSpan}>10%</span>
             <span className={styles.roundTipSpan}>${(fullProductCost*10/100).toFixed(2)}</span>
             </div>
             <div className={`${styles.roundPercent} ${selectedField==15 && styles.selectedPercent}`} 
-            onClick={()=>{setTipInputValue(""); setSelectedField(15); setTip((fullProductCost*15/100).toFixed(2));}}>
+            onClick={()=>{setTipError();setTipInputValue(""); setSelectedField(15); setTip((fullProductCost*15/100).toFixed(2));}}>
             <span className={styles.roundPercentSpan}>15%</span>
             <span className={styles.roundTipSpan}>${(fullProductCost*15/100).toFixed(2)}</span>
             </div>
@@ -70,7 +100,7 @@ export default function Tip({products, tip, setTip}){
                 setTipInputValue(tipValue);
                 return;
             }            
-
+            setTipError();
             const floatValue = parseFloat(tipValue);
             if(isNaN(floatValue) || /^-?\d*\.\d{3,}$/.test(tipValue))return;
         
@@ -91,6 +121,7 @@ export default function Tip({products, tip, setTip}){
         </div>
         <span className={`${styles.addTip} ${tipInputValue=="" && styles.addTipDisabled}`}
         onClick={()=>{
+          if(parseFloat(tipInputValue, 2) > fullProductCost) {setTipInputValue(""); setTipError(true); return;}
             setSelectedField(0);
             if(tipInputValue=="")setTip(0);
             else setTip(parseFloat(tipInputValue).toFixed(2));
@@ -99,7 +130,9 @@ export default function Tip({products, tip, setTip}){
 
 
         </div>
+        {tipError && <span className={styles.tipError}>Enter a tip less than ${fullProductCost}</span>}
             <span className={styles.thankYouMessage}>Thank you for supporting us.</span>
 
     </div>
+     </div>
 }
