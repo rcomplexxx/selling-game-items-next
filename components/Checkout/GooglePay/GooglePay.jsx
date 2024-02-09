6b50b82ea525b5ec05460d251b2119f1a54d726c
rcomplexxx <rcomplexx@gmail.com>
2024-02-09 16:54:59 +0100

    :gpay color changed

--- components/Checkout/GooglePay/GooglePay.jsx
+++ components/Checkout/GooglePay/GooglePay.jsx
@@ -191,7 +191,7 @@ const GooglePay = ({
       className={classNames(styles.gpayButton)}
       buttonType="plain"
       buttonSizeMode="fill"
-      buttonColor="white"
+      buttonColor="black"
       paymentRequest={{
         apiVersion: 2,
         apiVersionMinor: 0,
