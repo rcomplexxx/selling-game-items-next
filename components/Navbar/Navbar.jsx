154ebb651f52feaa32962d6af157e82ee6fcd8a0
rcomplexxx <rcomplexx@gmail.com>
2024-02-13 22:43:18 +0100

    :design improvement

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -302,14 +302,14 @@ setSubMenu(subMenu==2?0:2);
           <div className={styles.rightOptions}>
 
                   <Search searchOpen={searchOpen} setSearchOpen={setSearchOpen}/>
-                  <Link href={(router.asPath=='/cart' && totalItems>0)?"/checkout":"/cart"}>
+                  <Link href="/cart">
                 <div className={styles.cartStyle}>
                   <Image height={32} width={32}
-                    src={(router.asPath=='/cart' && totalItems>0)?"/images/bagBlueCheckout7.png":"/images/bagBlue.png"}
+                    src="/images/bagBlue.png"
                     className={styles.bagImg}
                     alt="cart"
                   />
-                  {totalItems > 0 && router.asPath!=='/cart' && (
+                  {totalItems > 0 &&  (
                     <div className={styles.badgeDiv}>{totalItems}</div>
                   )}
                 </div>
