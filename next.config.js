e47faf5075116da481c4aa09b4fd9fa32905dd8f
rcomplexxx <rcomplexx@gmail.com>
2024-02-27 00:06:25 +0100

    :test

--- next.config.js
+++ next.config.js
@@ -1,18 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        scrollRestoration: true,
-      },
-      compression: true,
-      optimization: {
-        usedExports: true,  // Enable tree shaking by marking unused exports
-        minimize: true,     // Enable minification to further reduce bundle size
-        experimental: {
-          optimizeCss: true,
-        },
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-        
-      },
-}
 
-module.exports = nextConfig
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     experimental: {
+//         scrollRestoration: true,
+//       },
+//       compression: true,
+//       optimization: {
+//         usedExports: true,  // Enable tree shaking by marking unused exports
+//         minimize: true,     // Enable minification to further reduce bundle size
+//         experimental: {
+//           optimizeCss: true,
+//         },
+//       },
+
+//       plugins: [
+//         new BundleAnalyzerPlugin()
+//       ]
+
+// }
+
+// module.exports = nextConfig
+
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+})
--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -3,7 +3,7 @@ import Head from "next/head.js";
 
 // Load the About component dynamically
 
-const AdminDynamic = dynamic(() => import("../../components/Admin/Admin.jsx"));
+const AdminDynamic = dynamic(() => import("../../components/Admin/Admin.jsx"), { ssr: false });
 
 export default function AdminPage() {
   return  <AdminDynamic />;
