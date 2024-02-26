4b0e8fb3f55cf4ca9b10a6057240de161bd29aa1
rcomplexxx <rcomplexx@gmail.com>
2024-02-26 18:17:46 +0100

    :test

--- pages/_app.js
+++ pages/_app.js
@@ -163,7 +163,7 @@ const totalItems= useMemo(()=>{
         <Component {...pageProps} />
       </AppContext.Provider>
 
-      {showNav && <Footer />}
+      {/* {showNav && <Footer />} */}
     </div>
   );
 }
