4c738fac41bf6e8edd6bcf59ae19c3762c2faebb
rcomplexxx <rcomplexx@gmail.com>
2024-02-28 03:37:30 +0100

    :optimizing

---
+++ utils/fonts.ts
@@ -0,0 +1,17 @@
+
+import { Inter, EB_Garamond } from 'next/font/google'
+
+
+export const inter = Inter({
+    subsets: ['latin'],
+    weight: ['300', '400', '600'],
+    display: 'swap',
+    variable: '--font-inter',
+})
+
+export const eb_Garamond = EB_Garamond({
+    subsets: ['latin'],
+    weight: ['400', '500', '600'],
+    display: 'swap',
+    variable: '--font-eb_garamond',
+})
