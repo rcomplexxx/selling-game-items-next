7892ed5054ce92ea2a745fe32ee7b19ea80abb6b
rcomplexxx <rcomplexx@gmail.com>
2024-02-28 04:07:28 +0100

    :optimizing

--- next.config.js
+++ next.config.js
@@ -1,33 +1,37 @@
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const withCompression = require('next-compress')();
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        scrollRestoration: true,
+    
+          optimizeFonts: true,
+     
+      },
+      compression: true,
+      optimization: {
+        usedExports: true,  // Enable tree shaking by marking unused exports
+        minimize: true,     // Enable minification to further reduce bundle size
+        experimental: {
+          optimizeCss: true,
+        },
+      }, plugins: [
+        purgecss({
+          content: ['./**/*.html']
+        })
+      ]
+
+}
+
+module.exports = nextConfig
 
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     experimental: {
-//         scrollRestoration: true,
-//       },
-//       compression: true,
-//       optimization: {
-//         usedExports: true,  // Enable tree shaking by marking unused exports
-//         minimize: true,     // Enable minification to further reduce bundle size
-//         experimental: {
-//           optimizeCss: true,
-//         },
-//       },
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
 
-//       plugins: [
-//         new BundleAnalyzerPlugin()
-//       ]
-
-// }
-
-// module.exports = nextConfig
-
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-})
+// module.exports = withBundleAnalyzer({
+//   reactStrictMode: true,
+// })
