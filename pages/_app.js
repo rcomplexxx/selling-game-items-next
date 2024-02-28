9683e5d6889d5d0befdbbef12f88a4a0c9c00d37
rcomplexxx <rcomplexx@gmail.com>
2024-02-28 01:48:28 +0100

    :optimizing

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect,  useMemo, useRef } from "react";
 import { useRouter } from "next/router";
-// import sqlite3 from 'sqlite3';
+
 
 import "../styles/globals.css";
 import Navbar from "../components/Navbar/Navbar.jsx";
@@ -114,29 +114,6 @@ export default function App({ Component, pageProps }) {
 
 
 
-  // // ! Koristiti ovaj segment koda za pravljenje jedne tabele u
-  // // !sqllite koja se treba inicijalizovati samo jednom. Nakon
-  // // !inicijalizacije izbrisati ovaj segment koda
-  // // import sqlite3 from 'sqlite3';
-
-  //   const db = new sqlite3.Database('./data/database.db');
-
-  // // Create a table
-  // db.run('CREATE TABLE IF NOT EXISTS admincheck (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)');
-
-  // // Insert data into the table
-  // const username = 'rcomplexxx';
-  // const password = 'bleachbankairevolution98';
-
-  // db.run('INSERT INTO admincheck (username, password) VALUES (?, ?)', [username, password], function(err) {
-  //   if (err) {
-  //     return console.error('Error inserting data:', err.message);
-  //   }
-  //   console.log(`A row has been inserted with id ${this.lastID}`);
-  // });
-
-  // // Close the database connection
-  // db.close();
 
 const totalItems= useMemo(()=>{
   let s=0;
