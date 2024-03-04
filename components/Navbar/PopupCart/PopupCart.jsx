cb0ad114a9f0d9864b9ab4f4f43c12fd106e331a
rcomplexxx <rcomplexx@gmail.com>
2024-03-04 03:09:50 +0100

    :optimization

--- components/Navbar/PopupCart/PopupCart.jsx
+++ components/Navbar/PopupCart/PopupCart.jsx
@@ -13,94 +13,87 @@ export default function PopupCart({totalItems,newProduct, setNewProduct}){
 
 const router = useRouter();
 
-useEffect(()=>{
 
-
-
-
- //na popstate?
-
-
-
-   const handlePopState = (event)=>{
-   
-  
-     
-     setNewProduct();
-  
-   
-   }
-
-      
-
-
- if(newProduct){
-
-   window?.addEventListener("popstate", handlePopState);
- }
-
-   
-
-
-
-
-
-   return () => {
-   
-       window?.removeEventListener("popstate", handlePopState);
-     
-   };
-
-
-
-
-},[newProduct])
 
 useEffect(()=>{
  router.beforePopState((state) => {
-   if(newProduct){
+
    state.options.scroll = false;
      
    return false;
- }
-return true;
+
  });
-},[newProduct,router])
+},[router])
+
+
+
 
 useEffect(()=>{
+
+
   window.history.pushState(null, null, router.asPath);
   history.go(1);
 
-  return ()=>{history.back()}
+
+
+
+  const handlePopState = (event)=>{
+   
+  
+     
+    setNewProduct();
+ 
+  
+  }
+
+     
+
+
+  window?.addEventListener("popstate", handlePopState);
+
+
+
+
+  return ()=>{
+    //Doraditi za uslov ako je kliknuto back?
+    // history.back();
+    window?.removeEventListener("popstate", handlePopState);
+  
+  }
 },[])
 
 
 // useEffect(()=>{ popupCart.focus();},[])
 
-    return <div className={`${styles.cartPopup} ${styles.materialize}`} >
+    return <div className={`${styles.cartPopup}`} >
    <div  className={styles.contentWrapper}>
-  <div className={styles.cartPopupTitle}> <Image height={12} width={12} src='/images/smallCorrect.png' className={styles.smallCorrect}></Image> <h3>Item added to your cart</h3> </div>
- <div className={styles.cartPopupTitle}> <Image height={54} width={96} src={`/images/${newProduct.image}`} className={styles.productImage}/> <h2 className={styles.productTitle}>{newProduct.name}</h2></div>
+  <div className={styles.cartPopupTitle}>
+     <Image height={12} width={12} src='/images/smallCorrect.png' className={styles.smallCorrect}/>
+     <h3>Item added to your cart</h3>
+      </div>
+ <div className={styles.cartPopupTitle}> 
+ <Image height={54} width={96} src={`/images/${newProduct.image}`} className={styles.productImage}/> 
+ <h2 className={styles.productTitle}>{newProduct.name}</h2>
+ </div>
  </div>
  
- <div className={styles.popupButtons}>
-  <Link href='/cart'>
- <button
-      className={styles.add_to_cart_button}
-      onMouseDown={(event)=>{event.preventDefault()}}
-     onClick={()=>{setNewProduct();}}
-    >
+
+  <Link href='/cart'  className={styles.add_to_cart_button}
+      onMouseDown={(event)=>{event.preventDefault()}}>
+
       View my cart ({totalItems})
-    </button>
+    
     </Link>
 
-    <Link href='/checkout'>
-    <button className={styles.but_now_button}  onMouseDown={(event)=>{event.preventDefault()}} onClick={()=>{setNewProduct();}}>
+    <Link href='/checkout' className={styles.buyNowButton}  onMouseDown={(event)=>{event.preventDefault()}} >
+    
      Check out
-    </button>
+  
     </Link>
-    <div className={styles.cartPopupTitle}  onMouseDown={(event)=>{event.preventDefault()}} onClick={()=>{setNewProduct();}}> <span className={styles.continue_shopping}>Continue shopping</span></div>
+    
+    <span className={styles.continue_shopping}  onClick={()=>{setNewProduct();}} onMouseDown={(event)=>{event.preventDefault()}} >Continue shopping</span>
+    
 
-  </div>
+ 
   </div>
 }
