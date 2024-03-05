112c3cae91c007fcb1c2257969246581cce62696
rcomplexxx <rcomplexx@gmail.com>
2024-03-05 01:37:25 +0100

    :optimized

--- pages/_app.js
+++ pages/_app.js
@@ -13,8 +13,6 @@ import { inter, eb_Garamond } from "@/utils/fonts";
 export default function App({ Component, pageProps }) {
   const [cartProducts, setCartProducts] = useState([]);
   const [newProduct, setNewProduct]=useState();
-  const [showNav, setShowNav] = useState(true);
-  const [removeNavFinal, setRemoveNavFinal]= useState(false);
   const [emailPopup, setEmailPopup] = useState(false);
 
   const router = useRouter();
@@ -23,14 +21,7 @@ export default function App({ Component, pageProps }) {
   useEffect(() => {
 
   
-    const handleRouteChangeComplete = (url) => {
-      url.startsWith("/checkout") ||
-      url === "/thank-you" ||
-      
-      url.startsWith("/admin")
-        ? setRemoveNavFinal(true)
-        : setRemoveNavFinal(false);
-    }
+  
     
 
     const handleRouteChangeStart = (url) => {
@@ -38,11 +29,7 @@ export default function App({ Component, pageProps }) {
       
       // 'url' parameter contains the new route
       setNewProduct();
-      if(url.startsWith("/checkout") ||
-      url === "/thank-you" ||
       
-      url.startsWith("/admin")){ setShowNav(false);}
-        else{ setShowNav(true); setRemoveNavFinal(false);}
       console.log('Route change started to:', url);
     };
 
@@ -54,9 +41,6 @@ export default function App({ Component, pageProps }) {
     //NAPRAVITI SMOOTH ANIMACIJU ZATVARANJA NAVBARA CIM SE KLIKNE NA LINK KOJI NE TREBA DA SADRZI NAVBAR
 
 
-    // Cleanup the event listener when the component unmounts
-   
-    //beforeHistoryChange
 
 
 
@@ -79,24 +63,23 @@ export default function App({ Component, pageProps }) {
   }, [cartProducts]);
 
   useEffect(() => {
-    const handleRouteChangeComplete = (url) => {
-      url.startsWith("/checkout") ||
-      url === "/thank-you" ||
-      
-      url.startsWith("/admin")
-        ? setRemoveNavFinal(true)
-        : setRemoveNavFinal(false);
-    };
+  
     
-    handleRouteChangeComplete(router.pathname);
 
 
 
-    clearInterval(popupTimeout.current); popupTimeout.current= setInterval(()=>{
+    clearInterval(popupTimeout.current); 
+    
+    
+    popupTimeout.current= setInterval(()=>{
+
       console.log('lstr', localStorage.getItem("popupShownDateInDays"), Math.floor(Date.now() / 86400000) )
-      if(localStorage.getItem("popupShownDateInDays") && (Math.floor(Date.now() / 86400000))-localStorage.getItem("popupShownDateInDays")<15 ){ clearTimeout(popupTimeout.current); return;}
-        console.log(router);
-    if(router.pathname!=='/404' && (router.pathname==='/' || (router.pathname.includes('/products') && !router.asPath.includes('#zoom')
+
+     
+       
+    if(localStorage.getItem("popupShownDateInDays") && (Math.floor(Date.now() / 86400000))-localStorage.getItem("popupShownDateInDays")>15  &&
+      
+      router.pathname!=='/404' && (router.pathname==='/' || (router.pathname.includes('/products') && !router.asPath.includes('#zoom')
     && !router.asPath.includes('#write-review')) || router.pathname.includes('/collection') || router.pathname=='/our-story' || router.pathname=='/faq')){
       setEmailPopup(true);  clearTimeout(popupTimeout.current);
     }
@@ -135,20 +118,19 @@ const totalItems= useMemo(()=>{
        <Head>
       <title>Gamebuff</title>
         <link rel="icon" href="/images/favicon.ico" />
-         {/* <link rel="stylesheet" href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap' />
-        <link rel="stylesheet" href='https://fonts.googleapis.com/css2?family=EB+Garamond:wght@300;400;600&display=swap' /> */}
+     
         </Head>
        
       
       {emailPopup && <EmailFlowPopup setEmailPopup={setEmailPopup}/>}
-      {!removeNavFinal && <Navbar totalItems={totalItems}  newProduct={newProduct} setNewProduct={setNewProduct}/>}
+     <Navbar totalItems={totalItems}  newProduct={newProduct} setNewProduct={setNewProduct}/>
 
       
       <AppContext.Provider value={{ cartProducts, setCartProducts, setNewProduct }}>
         <Component {...pageProps} />
       </AppContext.Provider>
       
-      {showNav && <Footer />}
+       <Footer />
     </div>
    
      
