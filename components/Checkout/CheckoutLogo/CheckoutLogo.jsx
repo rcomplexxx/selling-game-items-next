004b21533e75c4ed6b0a0ab62d01ff3fdc875d83
rcomplexxx <rcomplexx@gmail.com>
2024-03-29 20:30:58 +0100

    :feature added

--- components/Checkout/CheckoutLogo/CheckoutLogo.jsx
+++ components/Checkout/CheckoutLogo/CheckoutLogo.jsx
@@ -4,11 +4,25 @@ import Image from "next/image";
 
 export default function CheckoutLogo() {
   return (
+    <div className={styles.logoWrapper}>
     <div className={styles.logoDiv}>
-      
+      <Link href='/cart' className={styles.cartLink}>
+      <Image
+                  height={0}
+                  width={0}
+                  sizes="48px"
+                  src="/images/bagBlue.png"
+                  className={styles.bagImg}
+                  alt="cart"
+                />
+
+      </Link>
       <Link href='/'>
       <Image className={styles.logo} src="/images/commerce.png" height={0} width={0} sizes={'80px'}/>
       </Link>
+
+    
+    </div>
     </div>
   );
 }
